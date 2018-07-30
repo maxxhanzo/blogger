9a4d1fb53418b52f4aff6c1c8c9fd024d511846c
matwar <matwarbisht@gmail.com>
2018-07-31 00:51:29 +0530

    first

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = '?key=sdgfsdg1223';
+export function fetchPosts(){
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+	return {
+		type: "FETCH_POSTS",
+		payload: request
+	};
+}
